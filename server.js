a104858724ec13090a82c39cff9c3b23d21068cf
Marcus Santos <marcusdcarli@gmail.com>
2018-11-27 06:35:41 -0200

    Adicionando a gitIgnore

--- server.js
+++ server.js
@@ -1,7 +1,44 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const servidor = http.createServer(function (req, resp) {
-
-    resp.end('<html><body><a href="http://www.alura.com.br">Site da Alura</a></body></html>')
+app.listen(3000, function () {
+    console.log(`servidor rodando na porta 30000`);
 });
-servidor.listen(3000);
+
+app.get('/', function (req, resp) {
+    resp.send(
+        `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Casa do Código </h1>
+            </body>
+        </html>
+        `
+    );
+});
+
+app.get('/livros', function (req, resp) {
+    resp.send(
+        `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Listagem de Livros </h1>
+            </body>
+        </html>
+        `
+    );
+});
+
+// const http = require('http');
+
+// const servidor = http.createServer(function (req, resp) {
+
+//     resp.end('<html><body><a href="http://www.alura.com.br">Site da Alura</a></body></html>')
+// });
+// servidor.listen(3000);
