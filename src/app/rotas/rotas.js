58f16d1906054af37a33a76cc455c3607fb82f64
Marcus Santos <marcusdcarli@gmail.com>
2018-11-29 06:56:42 -0200

    Aula 3

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,3 +1,6 @@
+const LivroDao = require('../infra/livro-dao')
+const db = require('../../config/database');
+
 module.exports = (app) => {
     app.get('/', function (req, resp) {
         resp.send(
@@ -15,22 +18,18 @@ module.exports = (app) => {
     });
 
     app.get('/livros', function (req, resp) {
-        resp.marko(
+
+        const livroDao = new LivroDao(db);
+        livroDao.lista().then(livros => resp.marko(
             require('../views/livros/lista/lista.marko'),
             {
-                livros: [
-                    {
-                        id: 1,
-                        titulo: 'fundamentos do Node'
-                    },
-                    {
-                        id: 2,
-                        titulo: 'Node avançado'
-                    }
-                ]
+                livros: livros
             }
 
-        );
+        ))
+            .catch(erro => console.log(erro));
+
+
     });
 };
 
