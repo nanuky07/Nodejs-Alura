1a64e044b9b7766225ce2daed24d57417166cd94
Marcus Santos <marcusdcarli@gmail.com>
2018-12-03 06:22:53 -0200

    Aula 4

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -16,6 +16,92 @@ class LivroDao {
             )
         });
     }
+
+    adiciona(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSER INTO livros(
+                    titulo,
+                    preco,
+                    descricao
+                ) values(?,?,?)
+            `, [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
+
+                    resolve();
+                }
+            )
+        });
+    }
+
+    buscaPorId(id) {
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                SELECT *
+                FROM livros
+                WHERE id = ?
+            `, [id],
+                (erro, resultados) => {
+                    if (erro) {
+                        return reject('Não foi possível encontrar o livro!');
+                    }
+
+                    return resolve(resultados);
+                }
+            );
+        });
+    }
+
+    atualiza(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+               UPDATE livros SET
+                titulo = ?,
+                preco = ?,
+                descricao = ?
+                WHERE id = ?
+            `, [
+                    livro.preco,
+                    livro.descricao,
+                    livro.titulo,
+                    livro.id
+                ],
+                erro => {
+                    if (erro) {
+                        return reject('Não foi possível atualizar o livro!');
+                    }
+
+                    resolve();
+                });
+        });
+    }
+
+    remove(id) {
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                DELETE
+                FROM livros
+                WHERE id = ? 
+            `, [id],
+                (erro) => {
+                    if (erro) {
+                        return reject('Não foi possível remover o livro!');
+                    }
+                    return resolve();
+                }
+            );
+        });
+    }
 }
 
 module.exports = LivroDao;
